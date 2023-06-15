aaa8e70574f1b9d4007b52f5735dc8baffdafaf2
parwezshariq <shariqparwez@outlook.com>
2023-06-15 20:42:58 +0530

    Added graphql schema for book, which has nested data

---
+++ src/main/java/com/shariqparwez/graphql/component/fake/FakeBookDataResolver.java
@@ -0,0 +1,28 @@
+package com.shariqparwez.graphql.component.fake;
+
+import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsData;
+import com.netflix.graphql.dgs.InputArgument;
+import com.shariqparwez.graphql.datasource.fake.FakeBookDataSource;
+import com.shariqparwez.graphql.generated.types.Book;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@DgsComponent
+public class FakeBookDataResolver {
+
+    @DgsData(parentType = "Query", field = "books")
+    public List<Book> booksWrittenBy(@InputArgument(name = "author") Optional<String> authorName) {
+        if (authorName.isEmpty() || StringUtils.isBlank(authorName.get())) {
+            return FakeBookDataSource.BOOK_LIST;
+        }
+
+        return FakeBookDataSource.BOOK_LIST.stream()
+                .filter(book -> StringUtils.containsIgnoreCase(
+                        book.getAuthor().getName(), authorName.get()))
+                .collect(Collectors.toList());
+    }
+}
